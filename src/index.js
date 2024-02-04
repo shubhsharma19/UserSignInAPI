b343f20fccf719138d1eb5309faaea5f231264a8
Shubh Sharma <mail.shubhsharma19@gmail.com>
2024-02-04 14:04:35 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,41 +1,34 @@
+// task 1 = create new endpoint for /signup ✅
+// task 2 = make it so users can signup and a new token is generated for them 
+// task 3 = make the data save into mongo db
+// task 4 = make signin possible with token + make it find users from the db
+// task 5 = if user doesnt exist return error 
+// task 6 = if user exists return list of users in db
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 const { z } = require('zod');
+const jwtPass = "12345";
 
 const app = express();
-const port = 3000;
-const jwtPass = '12345';
-
-// dummy data
-// const USERSDATA = [
-//   {
-//     username: "silentknight",
-//     password: "god123",
-//     fullname: "aman trivedi",
-//   },
-//   {
-//     username: "johndoe",
-//     password: "123123",
-//     fullname: "john doe",
-//   },
-//   {
-//     username: "sumitbhai123",
-//     password: "sumit123",
-//     fullname: "sumit kumar",
-//   },
-//   {
-//     username: "shubhsig",
-//     password: "shubh12",
-//     fullname: "shubh sharma",
-//   }
-// ]
+const PORT = 3000;
+mongoose.connect("mongodb+srv://admin:admin123@cluster0.lhwf1g5.mongodb.net/user_app");
 
 // schema for input validation
 const loginInputSchema = z.object({
   body: z.object({
-    username: z.string().min(6),
-    password: z.string().min(6),
+    username: z.string().min(8),
+    email: z.string().endsWith('@gmail.com') || 
+    z.string().endsWith('.com'),
+    password: z.string().min(8),
   }),
+});
+
+// mongoose schema
+const User = mongoose.model('users', {
+ username: String,
+ email: String, 
+ password: String 
 });
 
 // middleware for input validation
@@ -53,11 +46,35 @@ const validateUserInput = (schema) => (req, res, next) => {
 app.use(express.json(), validateUserInput(loginInputSchema));
 
 // function to find if the user exists
-const userexists = (username, password) => {
-  return USERSDATA.find(user =>
-    user.username === username &&
-    user.password === password);
+async function userexists (email) {
+  const existUser = await db.users.findOne({ email: email });
+  return existUser;
 }
+
+app.post('/signup', (req, res) => {
+  const { userName, email, password} = req.body;
+
+  if (userexists(email)) {
+    return res.status(403).json({
+      message: "Account already exists, please sign in!",
+    });
+  } else {
+    // create a new user
+    const user = new User({
+      username: userName,
+      email: email,
+      password: password,
+    });
+    // save the user
+    user.save().then(() => console.log("Added User!"));
+    const token = jwt.sign({ email }, jwtPass);
+    // sending token to the user
+    return res.status(200).json({
+      message: "Signup successful!",
+      token,
+    });
+  }
+})
 
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
@@ -105,4 +122,6 @@ app.use((err, req, res, next) => {
   })
 })
 
-app.listen(port);
+app.listen(PORT, () => {
+  console.log(`Server is running at 3000`)
+});
